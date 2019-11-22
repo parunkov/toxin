fb2f731ae74dde522a4ced62da10e66ea61a8d69
parunkov <parunkov@gmail.com>
2019-11-22 10:08:58 +0500

    скопировал с курса

---
+++ source/pages/index/index.js
@@ -0,0 +1,8 @@
+import './index.scss';
+import 'normalize.css';
+
+import createMenu from '../../components/menu/menu';
+var menu = createMenu(['Главная','Блог'], 'menu');
+document.body.appendChild(menu);
+ 
+console.log('in index.js')
