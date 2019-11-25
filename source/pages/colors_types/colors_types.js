b82f5dd1e5a08daa5b63a475b0f7ab4ac8edc567
parunkov <parunkov@gmail.com>
2019-11-25 16:34:53 +0500

    Понедельник промежуточный

--- source/pages/colors_types/colors_types.js
+++ source/pages/colors_types/colors_types.js
@@ -1,5 +1,6 @@
 import '../../../node_modules/reset-css/sass/_reset.scss';
 import '../../components/page/page.js';
+import '../../components/grid/grid.js';
 import './colors_types.scss';
  
 // console.log('in !!!')
