2893eb855f310be012c8b78a20d27ec9c24fce99
parunkov <parunkov@gmail.com>
2019-11-25 14:12:53 +0500

    Делаю контейнер

--- source/pages/colors_types/colors_types.js
+++ source/pages/colors_types/colors_types.js
@@ -1,5 +1,5 @@
 import '../../../node_modules/reset-css/sass/_reset.scss';
-import '../../components/grid/grid.js';
+import '../../components/page/page.js';
 import './colors_types.scss';
  
-console.log('in !!!')
+// console.log('in !!!')
