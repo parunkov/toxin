284b9e16aacda2451f819ada1afba4fe890fd5d9
parunkov <parunkov@gmail.com>
2019-11-28 14:34:11 +0500

    Сверстал чекбокс

--- source/pages/form_elements/form_elements.js
+++ source/pages/form_elements/form_elements.js
@@ -3,5 +3,6 @@ import '../../variables.scss';
 import '../../components/page/page.js';
 import '../../components/ui_kit/ui_kit.js';
 import '../../components/input/input.js';
+import '../../components/checkbox/checkbox.js';
 import './form_elements.scss';
 // import './form_elements.pug';
