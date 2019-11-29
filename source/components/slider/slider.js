6439c060ca9b982482ac96c00c90b2eb6c95c3b6
parunkov <parunkov@gmail.com>
2019-11-29 17:15:48 +0500

    Пятница конец

--- source/components/slider/slider.js
+++ source/components/slider/slider.js
@@ -1,7 +1,6 @@
 import '../../variables.scss';
 import './slider.scss';
 import './slider.pug';
-// import '../../../../node_modules/jquery-ui/widgets/slider.js';
 import '../../../node_modules/jquery-ui/ui/widgets/slider.js';
 import $ from 'jquery';
 
@@ -9,12 +8,12 @@ $( function() {
     $( "#slider-range" ).slider({
       range: true,
       min: 0,
-      max: 500,
-      values: [ 75, 300 ],
+      max: 17000,
+      values: [ 5000, 10000 ],
       slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        $( "#amount" ).val( ui.values[ 0 ] + "₽" + " - " + ui.values[ 1 ]  + "₽");
       }
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) + "₽" +
+      " - " + $( "#slider-range" ).slider( "values", 1 ) + "₽");
   } );
