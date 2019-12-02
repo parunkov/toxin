a742d64590c1e0cf8512fb57bffb48ceeee8763c
parunkov <parunkov@gmail.com>
2019-12-02 09:54:35 +0500

    Переделал звезды

--- source/components/rate/rate.js
+++ source/components/rate/rate.js
@@ -2,3 +2,17 @@ import '../../variables.scss';
 import './rate.scss';
 import './rate.pug';
 
+let $active = $('.rate__active');
+	$('.rate__container').each(function(i) {
+		if ($active[i].innerHTML) {
+			let $rate = $(this);
+			let $stars = $rate.children();
+			$stars.each(function(i) {
+				$(this).click(function() {
+					$stars.html('star_border');
+					$stars.slice(0, i+1).html('star');
+				});
+			});
+		}
+	});
+
--- source/pages/form_elements/form_elements.js
+++ source/pages/form_elements/form_elements.js
@@ -13,17 +13,17 @@ import './form_elements.scss';
 // import './form_elements.pug';
 
 
-$('.form_elements__rate-1-star').each(function( i ) {
-	$(this).click(function() {
-		$('.form_elements__rate-1-star').text('star_border');
-		$('.form_elements__rate-1-star:lt('+(i+1)+')').text('star');
-	});
-});
-	$('.form_elements__rate-2-star').each(function( i ) {
-		$(this).click(function() {
-			$('.form_elements__rate-2-star').text('star_border');
-			$('.form_elements__rate-2-star:lt('+(i+1)+')').text('star');
-		});
-	});
+// $('.form_elements__rate-1-star').each(function( i ) {
+// 	$(this).click(function() {
+// 		$('.form_elements__rate-1-star').text('star_border');
+// 		$('.form_elements__rate-1-star:lt('+(i+1)+')').text('star');
+// 	});
+// });
+// 	$('.form_elements__rate-2-star').each(function( i ) {
+// 		$(this).click(function() {
+// 			$('.form_elements__rate-2-star').text('star_border');
+// 			$('.form_elements__rate-2-star:lt('+(i+1)+')').text('star');
+// 		});
+// 	});
 
 
