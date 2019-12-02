f86efb4fbe9ee667c37f6372670978943f74e319
parunkov <parunkov@gmail.com>
2019-12-02 10:44:22 +0500

    Сделал слайдер

--- source/components/slider/slider.js
+++ source/components/slider/slider.js
@@ -4,16 +4,23 @@ import './slider.pug';
 import '../../../node_modules/jquery-ui/ui/widgets/slider.js';
 import $ from 'jquery';
 
+function prettify (num) {
+    var n = num.toString();
+    var separator = " ";
+    return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + separator);
+}
+
 $( function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 17000,
-      values: [ 5000, 10000 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( ui.values[ 0 ] + "₽" + " - " + ui.values[ 1 ]  + "₽");
-      }
-    });
-    $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) + "₽" +
-      " - " + $( "#slider-range" ).slider( "values", 1 ) + "₽");
-  } );
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 17000,
+    step: 100,
+    values: [ 5000, 10000 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).val( prettify(ui.values[ 0 ]) + "₽" + " - " + prettify(ui.values[ 1 ])  + "₽");
+    }
+  });
+  $( "#amount" ).val( prettify($( "#slider-range" ).slider( "values", 0 )) + "₽" +
+    " - " + prettify($( "#slider-range" ).slider( "values", 1 ) + "₽"));
+} );
