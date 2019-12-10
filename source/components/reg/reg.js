068b97107155113d8bc4cabb814f1019e038bbc2
parunkov <parunkov@gmail.com>
2019-12-10 16:43:47 +0500

    Вторник конец

---
+++ source/components/reg/reg.js
@@ -0,0 +1,7 @@
+import '../../../node_modules/reset-css/sass/_reset.scss';
+import '../../variables.scss';
+import '../../components/input/input.js';
+import '../../components/btn/btn.js';
+import '../../components/radio/radio.js';
+import '../../components/toggle/toggle.js';
+import './reg.scss';
