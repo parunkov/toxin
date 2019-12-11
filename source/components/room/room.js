a92d9a4c932f3e40924012793b88626401c04999
parunkov <parunkov@gmail.com>
2019-12-11 10:43:44 +0500

    сверстал room

--- source/components/room/room.js
+++ source/components/room/room.js
@@ -4,3 +4,9 @@ import '../../components/input/input.js';
 import '../../components/dropdown/dropdown.js';
 import '../../components/btn/btn.js';
 import './room.scss';
+$(document).ready(() => {
+	$('.room__container .iqdropdown button').eq(1).trigger('click');
+	$('.room__container .iqdropdown button').eq(1).trigger('click');
+	$('.room__container .iqdropdown button').eq(3).trigger('click');
+	$('.room__container .dropdown__set').trigger('click');
+});
--- source/pages/cards/cards.js
+++ source/pages/cards/cards.js
@@ -7,9 +7,3 @@ import '../../components/reg/reg.js';
 import '../../components/room/room.js';
 import './cards.scss';
 
-// $(document).ready(() => {
-// 	$('.cards__content--3 .iqdropdown button').eq(1).trigger('click');
-// 	$('.cards__content--3 .iqdropdown button').eq(1).trigger('click');
-// 	$('.cards__content--3 .iqdropdown button').eq(3).trigger('click');
-// 	$('.cards__content--3 .dropdown__set').trigger('click');
-// });
