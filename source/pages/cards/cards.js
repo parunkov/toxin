ae17427bced6be87ace08501d2f9718967aab66c
parunkov <parunkov@gmail.com>
2019-12-11 11:07:55 +0500

    Сверстал логин

--- source/pages/cards/cards.js
+++ source/pages/cards/cards.js
@@ -5,5 +5,6 @@ import '../../components/ui_kit/ui_kit.js';
 import '../../components/search/search.js';
 import '../../components/reg/reg.js';
 import '../../components/room/room.js';
+import '../../components/login/login.js';
 import './cards.scss';
 
