f0e36782938d84f48b35dc4323e182d669c91cd3
parunkov <parunkov@gmail.com>
2019-12-12 14:43:01 +0500

    Делаю карточку

---
+++ source/components/card/card.js
@@ -0,0 +1,4 @@
+import '../../../node_modules/reset-css/sass/_reset.scss';
+import '../../variables.scss';
+import '../../components/rate/rate.js';
+import './card.scss';
--- source/pages/cards/cards.js
+++ source/pages/cards/cards.js
@@ -7,6 +7,7 @@ import '../../components/reg/reg.js';
 import '../../components/room/room.js';
 import '../../components/login/login.js';
 import '../../components/datepicker/datepicker.js';
+import '../../components/card/card.js';
 import './cards.scss';
 
 
