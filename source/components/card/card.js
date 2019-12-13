e12907fed4022235ca49a708ae8203d0adb4728a
parunkov <parunkov@gmail.com>
2019-12-13 09:46:21 +0500

    показ слайдов сделал функцию

--- source/components/card/card.js
+++ source/components/card/card.js
@@ -2,3 +2,46 @@ import '../../../node_modules/reset-css/sass/_reset.scss';
 import '../../variables.scss';
 import '../../components/rate/rate.js';
 import './card.scss';
+
+$('.card__container').each(function(i) {
+	let counter = 0;
+	let $slides = $('.card__container').eq(i).find('.card__slide');
+	// console.log($slides);
+	let left = $('.card__container').eq(i).find('.card__arrow-left');
+	let right = $('.card__container').eq(i).find('.card__arrow-right');
+	let $controls = $('.card__container').eq(i).find('.card__control');
+	// console.log($controls);
+	let showSlides = function() {
+		$slides.removeClass('card__slide-left');
+		$slides.removeClass('card__slide-active');
+		$slides.removeClass('card__slide-right');
+		$controls.removeClass('card__control--active');
+		switch (counter) {
+			case 0:
+				$slides.eq(3).addClass('card__slide-left');
+				$slides.eq(0).addClass('card__slide-active');
+				$slides.eq(1).addClass('card__slide-right');
+				$controls.eq(0).addClass('card__control--active');
+				break;
+			case 1:
+				$slides.eq(0).addClass('card__slide-left');
+				$slides.eq(1).addClass('card__slide-active');
+				$slides.eq(2).addClass('card__slide-right');
+				$controls.eq(1).addClass('card__control--active');
+				break;
+			case 2:
+				$slides.eq(1).addClass('card__slide-left');
+				$slides.eq(2).addClass('card__slide-active');
+				$slides.eq(3).addClass('card__slide-right');
+				$controls.eq(2).addClass('card__control--active');
+				break;
+			case 3:
+				$slides.eq(2).addClass('card__slide-left');
+				$slides.eq(3).addClass('card__slide-active');
+				$slides.eq(0).addClass('card__slide-right');
+				$controls.eq(3).addClass('card__control--active');
+				break;
+		}
+	}
+	showSlides();
+});
--- source/components/data/data.js
+++ source/components/data/data.js
@@ -1,1 +1,4 @@
 import './image1.jpg';
+import './image2.jpg';
+import './image3.jpg';
+import './image4.jpg';
