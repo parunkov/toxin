9f06195aaa468667e2a3d7ccbe26766fd567b786
parunkov <parunkov@gmail.com>
2019-12-16 09:28:13 +0500

    Сделал header

--- source/components/header/header.js
+++ source/components/header/header.js
@@ -2,3 +2,15 @@ import '../../../node_modules/reset-css/sass/_reset.scss';
 import '../../variables.scss';
 import '../../components/btn/btn.js';
 import './header.scss';
+
+$('.header__container').each(function(i) {
+	let num = $('.header__container').eq(i).find('.header__active').html();
+	console.log(num);
+	$('.header__container').eq(i).find('.header__li').each(function(j) {
+		console.log(j);
+		if (j == num) {
+			$('.header__container').eq(i).find('.header__li').eq(j).addClass('header__li--active');
+			$('.header__container').eq(i).find('.header__li').eq(j).find('a').addClass('header__a--active');
+		}
+	});
+});
