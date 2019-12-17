8e19f16878d6e753e1aaec8309cb4ae0b4362218
parunkov <parunkov@gmail.com>
2019-12-17 10:31:35 +0500

    Исправил хедер, сделал индекс форму

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -2,5 +2,6 @@ import '../../../node_modules/reset-css/sass/_reset.scss';
 import '../../variables.scss';
 import '../../components/page/page.js';
 import '../../components/page_header/page_header.js';
+import '../../components/search/search.js';
 import './index.scss';
 
