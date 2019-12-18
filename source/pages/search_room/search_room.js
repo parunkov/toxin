baf7e3977645956fa9cfa1b79592656e8530e1b2
parunkov <parunkov@gmail.com>
2019-12-18 11:42:27 +0500

    Сверстал Searh Room

--- source/pages/search_room/search_room.js
+++ source/pages/search_room/search_room.js
@@ -9,6 +9,7 @@ import '../../components/checkbox/checkbox.js';
 import '../../components/card/card.js';
 import '../../components/page_footer/page_footer.js';
 import '../../components/datepicker/datepicker.js';
+import '../../components/pagination/pagination.js';
 import './search_room.scss';
 
 let myDatepicker = $('.search_room__datepicker-container .datepicker__content').datepicker().data('datepicker');
@@ -47,3 +48,14 @@ $('.search_room__expandable-h3').click(function(evt) {
 $('.search_room__checkbox-1:gt(0)').prop('checked', true);
 $('.search_room__checkbox-3:lt(4)').prop('checked', true);
 $('.search_room__checkbox-3').eq(0).prop('checked', false);
+
+$(document).ready(() => {
+	$('.search_room__dropdown-wrap .iqdropdown button').eq(1).trigger('click');
+	$('.search_room__dropdown-wrap .iqdropdown button').eq(1).trigger('click');
+	$('.search_room__dropdown-wrap .iqdropdown button').eq(3).trigger('click');
+	$('.search_room__dropdown-wrap .iqdropdown button').eq(5).trigger('click');
+	$('.search_room__dropdown-2-wrap .iqdropdown button').eq(1).trigger('click');
+	$('.search_room__dropdown-2-wrap .iqdropdown button').eq(1).trigger('click');
+	$('.search_room__dropdown-2-wrap .iqdropdown button').eq(3).trigger('click');
+	$('.search_room__dropdown-2-wrap .iqdropdown button').eq(3).trigger('click');
+});
