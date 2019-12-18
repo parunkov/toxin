4ae9c44488ba3458badfa5c63300e68b75e14992
parunkov <parunkov@gmail.com>
2019-12-18 15:51:26 +0500

    Сделал картинки

--- source/pages/room_details/room_details.js
+++ source/pages/room_details/room_details.js
@@ -4,3 +4,6 @@ import '../../components/page/page.js';
 import '../../components/page_header/page_header.js';
 import '../../components/page_footer/page_footer.js';
 import './room_details.scss';
+import './room1.jpg';
+import './room2.jpg';
+import './room3.jpg';
