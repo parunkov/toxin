7435fea82a9891c1f199ecdc621b5f1a60c9dfe8
parunkov <parunkov@gmail.com>
2019-12-19 12:10:01 +0500

    Сделал диаграмму

--- source/pages/room_details/room_details.js
+++ source/pages/room_details/room_details.js
@@ -8,3 +8,89 @@ import './room_details.scss';
 import './room1.jpg';
 import './room2.jpg';
 import './room3.jpg';
+
+let dataset1 = [
+	{
+		value: 0.5,
+		color: '#ffffff'
+	},
+	{
+		value: 24,
+		color: '#b29dfe'
+	}, 
+	{
+		value: 1,
+		color: '#ffffff'
+	},
+	{
+		value: 24,
+		color: '#6ed0a3'
+	}, 
+	{
+		value: 1,
+		color: '#ffffff'
+	},
+	{
+		value: 24,
+		color: '#ffcd9c'
+	},
+	{
+		value: 1,
+		color: '#ffcd9c'
+	},
+	{
+		value: 24,
+		color: '#ffcd9c'
+	}
+];
+let dataset2 = [
+	{
+		value: 0.5,
+		color: '#ffffff'
+	},
+	{
+		value: 24,
+		color: '#b29dfe'
+	}
+];
+let addCart = function(dataset, chartClass, sectorClass) {
+	let maxValue = 25;
+	let container = $(chartClass);
+	
+	let addSector = function(data, startAngle, collapse) {
+		let sectorDeg = 3.6 * data.value;
+		let skewDeg = 90 + sectorDeg;
+		let rotateDeg = startAngle;
+		if (collapse) {
+			skewDeg++;
+		}
+		
+		let sector = $('<div>', {
+			'class': sectorClass
+		}).css({
+			'background': data.color,
+			'transform': 'rotate(' + rotateDeg + 'deg) skewY(' + skewDeg + 'deg)'
+		});
+		container.append(sector);
+		
+		return startAngle + sectorDeg;
+	};
+	
+	dataset.reduce(function (prev, curr) {
+		return (function addPart(data, angle) {
+			if (data.value <= maxValue) {
+				return addSector(data, angle, false);
+			}
+			
+			return addPart({
+				value: data.value - maxValue,
+				color: data.color
+			}, addSector({
+				value: maxValue,
+				color: data.color,
+			}, angle, true));
+		})(curr, prev);
+	}, 0);
+}
+addCart(dataset1, '.room_details__chart', 'room_details__sector');
+addCart(dataset2, '.room_details__chart-2', 'room_details__sector');
