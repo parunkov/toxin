f92efea911fc03ee80ae613b37ae52639d4ab5fe
parunkov <parunkov@gmail.com>
2020-04-08 10:17:37 +0500

    Исправил инпут и ссылку в футере. Сделал выпадающее меню по клику #5

--- source/components/header/header.js
+++ source/components/header/header.js
@@ -5,9 +5,7 @@ import './header.scss';
 
 $('.header__container').each(function(i) {
 	let num = $('.header__container').eq(i).find('.header__active').html();
-	// console.log(num);
 	$('.header__container').eq(i).find('.header__li').each(function(j) {
-		// console.log(j);
 		if (j == num) {
 			$('.header__container').eq(i).find('.header__li').eq(j).addClass('header__li--active');
 			$('.header__container').eq(i).find('.header__li').eq(j).find('a').addClass('header__a--active');
@@ -15,17 +13,48 @@ $('.header__container').each(function(i) {
 	});
 });
 
-$('.header__li--big').hover(
-	function(){
-		$('.header__ul-incl').removeClass('header__ul-incl--show');
-		$('.header__arrow').text('expand_more');
-		$(this).find('.header__ul-incl').addClass('header__ul-incl--show');
-		$(this).find('.header__arrow').text('expand_less');
-		$(document).click(
-			function(){
+let openedMenu;
+let isOpen = false;
+
+$('.header__li--big a').click(
+	function(e){
+		if (!isOpen) {
+			e.preventDefault();
+			$('.header__ul-incl').removeClass('header__ul-incl--show');
+			$('.header__arrow').text('expand_more');
+			$(this).parent().find('.header__ul-incl').addClass('header__ul-incl--show');
+			$(this).parent().find('.header__arrow').text('expand_less');
+			isOpen = true;
+			openedMenu = this;
+		}
+	}
+);
+$(document).click(
+	function(e) {
+		if (e.target != openedMenu) {
+			if ($(e.target).parent().hasClass('header__li--big')) {
+				e.preventDefault();
+				console.log(isOpen);
+			}
+			if (isOpen) {
 				$('.header__ul-incl').removeClass('header__ul-incl--show');
 				$('.header__arrow').text('expand_more');
+				isOpen = false;
+				$(e.target).parent().find('.header__ul-incl').addClass('header__ul-incl--show');
+				$(e.target).parent().find('.header__arrow').text('expand_less');
 			}
-		)
+		}
+	}
+);
+$('.header__arrow').click(
+	function() {
+		if ($(this).text() === 'expand_more') {
+			console.log(1);
+			$(this).parent().find('.header__ul-incl').addClass('header__ul-incl--show');
+			$(this).parent().find('.header__arrow').text('expand_less');
+		} else {
+			$(this).parent().find('.header__ul-incl').removeClass('header__ul-incl--show');
+			$(this).parent().find('.header__arrow').text('expand_more');
+		}
 	}
 );
