86373ef78f04e36b684d0215e989666176adb094
parunkov <parunkov@gmail.com>
2020-04-09 08:20:35 +0500

    Исправил скрипт меню #5

--- source/components/header/header.js
+++ source/components/header/header.js
@@ -13,48 +13,31 @@ $('.header__container').each(function(i) {
 	});
 });
 
-let openedMenu;
-let isOpen = false;
 
-$('.header__li--big a').click(
-	function(e){
-		if (!isOpen) {
+$('.header__li--big').click(
+	function(e) {
+		if ($(e.target).parent().hasClass('header__li--big')) {
 			e.preventDefault();
+		}
+		if ($(this).find('.header__ul-incl').hasClass('header__ul-incl--show')) {
 			$('.header__ul-incl').removeClass('header__ul-incl--show');
 			$('.header__arrow').text('expand_more');
-			$(this).parent().find('.header__ul-incl').addClass('header__ul-incl--show');
-			$(this).parent().find('.header__arrow').text('expand_less');
-			isOpen = true;
-			openedMenu = this;
+		} else {
+			$('.header__ul-incl').removeClass('header__ul-incl--show');
+			$('.header__arrow').text('expand_more');
+			$(this).find('.header__ul-incl').addClass('header__ul-incl--show');
+			$(this).find('.header__arrow').text('expand_less');
 		}
 	}
 );
 $(document).click(
 	function(e) {
-		if (e.target != openedMenu) {
-			if ($(e.target).parent().hasClass('header__li--big')) {
-				e.preventDefault();
-				console.log(isOpen);
-			}
-			if (isOpen) {
-				$('.header__ul-incl').removeClass('header__ul-incl--show');
-				$('.header__arrow').text('expand_more');
-				isOpen = false;
-				$(e.target).parent().find('.header__ul-incl').addClass('header__ul-incl--show');
-				$(e.target).parent().find('.header__arrow').text('expand_less');
-			}
-		}
-	}
-);
-$('.header__arrow').click(
-	function() {
-		if ($(this).text() === 'expand_more') {
-			console.log(1);
-			$(this).parent().find('.header__ul-incl').addClass('header__ul-incl--show');
-			$(this).parent().find('.header__arrow').text('expand_less');
+		if ($(e.target).parent().hasClass('header__li--big')) {
+			e.preventDefault();
 		} else {
-			$(this).parent().find('.header__ul-incl').removeClass('header__ul-incl--show');
-			$(this).parent().find('.header__arrow').text('expand_more');
+			$('.header__ul-incl').removeClass('header__ul-incl--show');
+			$('.header__arrow').text('expand_more');
 		}
 	}
 );
+
