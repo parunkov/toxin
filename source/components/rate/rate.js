aa7131dc43cd5bcc47e9544e4d63c4220323c837
parunkov <parunkov@gmail.com>
2020-04-24 15:29:40 +0500

    Исправил rate.js по стандарту

--- source/components/rate/rate.js
+++ source/components/rate/rate.js
@@ -2,17 +2,16 @@ import '../../variables.scss';
 import './rate.scss';
 import './rate.pug';
 
-let $active = $('.rate__active');
-	$('.rate__container').each(function(i) {
-		if ($active[i].innerHTML) {
-			let $rate = $(this);
-			let $stars = $rate.children();
-			$stars.each(function(i) {
-				$(this).click(function() {
-					$stars.html('star_border');
-					$stars.slice(0, i+1).html('star');
-				});
-			});
-		}
-	});
-
+const $active = $('.rate__active');
+$('.rate__container').each(function (i) {
+  if ($active[i].innerHTML) {
+    const $rate = $(this);
+    const $stars = $rate.children();
+    $stars.each(function (j) {
+      $(this).click(() => {
+        $stars.html('star_border');
+        $stars.slice(0, j + 1).html('star');
+      });
+    });
+  }
+});
