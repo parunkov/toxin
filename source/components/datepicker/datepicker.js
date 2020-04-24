4bfe7ee4ef6eaa2b12d09189f257469df270a6c3
parunkov <parunkov@gmail.com>
2020-04-24 14:02:28 +0500

    Исправил datepicker.js по стандарту

--- source/components/datepicker/datepicker.js
+++ source/components/datepicker/datepicker.js
@@ -1,63 +1,63 @@
 import '../../../node_modules/reset-css/sass/_reset.scss';
 import '../../variables.scss';
-import '../../components/datepicker/datepicker_plugin.js';
-import '../../components/datepicker/datepicker.css';
+import './datepicker_plugin.js';
+import './datepicker.css';
 import './datepicker.scss';
 
 $('.datepicker__content').datepicker({
-	minDate: new Date(),
-	range: true,
-	navTitles: {
-	    days: 'MM <i>yyyy</i>',
-	    months: 'yyyy',
-	    years: 'yyyy1 - yyyy2'
-	},
-	dateFormat: 'dd.mm.yyyy',
+  minDate: new Date(),
+  range: true,
+  navTitles: {
+    days: 'MM <i>yyyy</i>',
+    months: 'yyyy',
+    years: 'yyyy1 - yyyy2',
+  },
+  dateFormat: 'dd.mm.yyyy',
 });
 
 
-let myDatepickers = [];
+const myDatepickers = [];
 // window.datepickers = {
-// 	i: [],
-// 	dates: []
+//  i: [],
+//  dates: []
 // };
 
-$('.datepicker__content').each(function(i) {
-	myDatepickers[i] = $('.datepicker__content').eq(i).datepicker().data('datepicker');
-	// myDatepickers[i].mouseup(function() {
-	// 	console.log(myDatepickers[i].selectedDates);
-	// });
+$('.datepicker__content').each((i) => {
+  myDatepickers[i] = $('.datepicker__content').eq(i).datepicker().data('datepicker');
+  // myDatepickers[i].mouseup(function() {
+  //  console.log(myDatepickers[i].selectedDates);
+  // });
 });
-let $clear = $('.datepicker__clear');
-$('.datepicker__set').each(function(i) {
-	$('.datepicker__set').eq(i).click(function(evt) {
-		evt.preventDefault();
-		// console.log(myDatepickers[i].selectedDates);
-		// $clear.eq(i).css({'display' : 'block'});
-		// console.log(myDatepickers[i].selectedDates);
-		// window.datepickers.i[i] = i;
-		// window.datepickers.dates[i] = myDatepickers[i].selectedDates;
-		// console.log(window.datepickers);
-	});
-}); 
-$clear.each(function(i) {
-	$clear.eq(i).click(function(evt) {
-		evt.preventDefault();
-		myDatepickers[i].clear();
-		$clear.eq(i).css({'display' : 'none'});
-		// console.log(myDatepickers[i].selectedDates);
-		// window.datepickers.i[i] = i;
-		// window.datepickers.dates[i] = myDatepickers[i].selectedDates;
-		// console.log(window.datepickers);
-	});
+const $clear = $('.datepicker__clear');
+$('.datepicker__set').each((i) => {
+  $('.datepicker__set').eq(i).click((evt) => {
+    evt.preventDefault();
+    // console.log(myDatepickers[i].selectedDates);
+    // $clear.eq(i).css({'display' : 'block'});
+    // console.log(myDatepickers[i].selectedDates);
+    // window.datepickers.i[i] = i;
+    // window.datepickers.dates[i] = myDatepickers[i].selectedDates;
+    // console.log(window.datepickers);
+  });
 });
-$('.datepicker__container').each(function(i) {
-	// myDatepickers[i] = $('.datepicker__content').eq(i).datepicker().data('datepicker');
-	$(this).mouseup(function() {
-		if (myDatepickers[i].selectedDates.length === 0) {
-			$clear.eq(i).css({'display' : 'none'});
-		} else {
-			$clear.eq(i).css({'display' : 'block'});
-		}
-	});
+$clear.each((i) => {
+  $clear.eq(i).click((evt) => {
+    evt.preventDefault();
+    myDatepickers[i].clear();
+    $clear.eq(i).css({ display: 'none' });
+    // console.log(myDatepickers[i].selectedDates);
+    // window.datepickers.i[i] = i;
+    // window.datepickers.dates[i] = myDatepickers[i].selectedDates;
+    // console.log(window.datepickers);
+  });
+});
+$('.datepicker__container').each(function (i) {
+  // myDatepickers[i] = $('.datepicker__content').eq(i).datepicker().data('datepicker');
+  $(this).mouseup(() => {
+    if (myDatepickers[i].selectedDates.length === 0) {
+      $clear.eq(i).css({ display: 'none' });
+    } else {
+      $clear.eq(i).css({ display: 'block' });
+    }
+  });
 });
