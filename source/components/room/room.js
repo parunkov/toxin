d393f88923f5cb5794855300e151632c7aad11c2
parunkov <parunkov@gmail.com>
2020-04-27 09:38:57 +0500

    Добавил datepicker в room_details

--- source/components/room/room.js
+++ source/components/room/room.js
@@ -4,6 +4,7 @@ import '../input/input';
 import '../dropdown/dropdown';
 import '../btn/btn';
 import './room.scss';
+import '../datepicker/datepicker';
 import $ from 'jquery';
 
 const $button = $('.room__container .iqdropdown button');
@@ -14,4 +15,57 @@ $(document).ready(() => {
   $button.eq(3).trigger('click');
   $('.room__container .dropdown__set').trigger('click');
   $('.room__container .iqdropdown').removeClass('menu-open');
+});
+
+const $myDatepicker = $('.room__datepicker-container .datepicker__content').datepicker().data('datepicker');
+const $datepickerContainer = $('.room__datepicker-container');
+const $setBtn = $('.room__datepicker-container .datepicker__set');
+const $clearBtn = $('.room__datepicker-container .datepicker__clear');
+const $arrival = $('.room__input-1');
+const $departure = $('.room__input-2');
+
+const $icon = $('.room__input-wrap .input__icon');
+const $iconText = $('.room__input-wrap .input__icon-text');
+
+const dateToValue = (date) => `${(`0${date.getDate()}`).substr(-2)}.${(`0${date.getMonth() + 1}`).substr(-2)}.${date.getFullYear()}`;
+
+$arrival.attr('disabled', '');
+$departure.attr('disabled', '');
+
+$('.room__container .input__icon').click(function (evt) {
+  evt.preventDefault();
+  if ($datepickerContainer.css('display') === 'block' && $(this).find('.input__icon-text').text() === 'expand_more') {
+    $datepickerContainer.css({ display: 'none' });
+    $iconText.text('expand_more');
+    $(this).find('.input__icon-text').text('expand_more');
+    $icon.css({ cursor: 'pointer' });
+  } else if ($datepickerContainer.css('display') === 'block' && $(this).find('.input__icon-text').text() === 'expand_less') {
+    $(this).find('.input__icon-text').text('expand_more');
+  } else if ($datepickerContainer.css('display') === 'none' && $(this).find('.input__icon-text').text() === 'expand_less') {
+    $datepickerContainer.css({ display: 'block' });
+    $iconText.text('expand_more');
+    $icon.css({ cursor: 'default' });
+    $(this).find('.input__icon-text').text('expand_less');
+    $(this).css({ cursor: 'pointer' });
+  }
+  $setBtn.click((e) => {
+    e.preventDefault();
+    $datepickerContainer.css({ display: 'none' });
+    $iconText.text('expand_more');
+    if ($myDatepicker.selectedDates[0]) {
+      $arrival.attr('value', dateToValue($myDatepicker.selectedDates[0]));
+    } else {
+      $arrival.attr('value', '');
+    }
+    if ($myDatepicker.selectedDates[1]) {
+      $departure.attr('value', dateToValue($myDatepicker.selectedDates[1]));
+    } else {
+      $departure.attr('value', '');
+    }
+  });
+  $clearBtn.click((e) => {
+    e.preventDefault();
+    $arrival.attr('value', '');
+    $departure.attr('value', '');
+  });
 });
