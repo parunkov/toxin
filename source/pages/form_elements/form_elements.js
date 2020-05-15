2e37d8a4e873d5a663f2138d4544f0990c8f7f12
parunkov <parunkov@gmail.com>
2020-05-15 15:48:30 +0500

    Исправил ошибки в дропдаунах в form_elements

--- source/pages/form_elements/form_elements.js
+++ source/pages/form_elements/form_elements.js
@@ -20,19 +20,19 @@ import $ from 'jquery';
 $('.form_elements__checkbox-1:gt(0)').prop('checked', true);
 $('.form_elements__toggle').eq(0).prop('checked', true);
 $('.form_elements__2-like').prop('checked', true);
-$('.form_elements__item--17 .iqdropdown').addClass('menu-open');
 $('.form_elements__checkbox-2:lt(4)').prop('checked', true);
 $('.form_elements__checkbox-2').eq(0).prop('checked', false);
-$('.form_elements__item--20 .iqdropdown').addClass('menu-open');
-$('.form_elements__item--22 .iqdropdown').addClass('menu-open');
 $('.form_elements__revew-1-like').prop('checked', true);
 $(document).ready(() => {
+
+  $('.input__icon-dropdown').eq(1).trigger('click');
+  $('.input__icon-dropdown').eq(2).trigger('click');
+  $('.input__icon-dropdown').eq(3).trigger('click');
+
   const $button = $('.form_elements__item--22 .iqdropdown button');
   $button.eq(1).trigger('click');
   $button.eq(1).trigger('click');
   $button.eq(3).trigger('click');
-  $('.form_elements__item--22 .dropdown__set').trigger('click');
-  $('.form_elements__item--22 .iqdropdown').addClass('menu-open');
 
   $('.form_elements__item--15').bind('DOMSubtreeModified', () => {
     const $paginationPage = $('.paginationjs-page');
