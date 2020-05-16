d37cbd5d4861dc62e3ce49d210429670087703b5
parunkov <parunkov@gmail.com>
2020-05-16 10:32:56 +0500

    Исправил form-elements по БЭМ

--- source/pages/form-elements/form-elements.js
+++ source/pages/form-elements/form-elements.js
@@ -14,27 +14,27 @@ import '../../components/pagination/pagination';
 import '../../components/dropdown/dropdown';
 import '../../components/info/info';
 import '../../components/review/review';
-import './form_elements.scss';
+import './form-elements.scss';
 import $ from 'jquery';
 
-$('.form_elements__checkbox-1:gt(0)').prop('checked', true);
-$('.form_elements__toggle').eq(0).prop('checked', true);
-$('.form_elements__2-like').prop('checked', true);
-$('.form_elements__checkbox-2:lt(4)').prop('checked', true);
-$('.form_elements__checkbox-2').eq(0).prop('checked', false);
-$('.form_elements__revew-1-like').prop('checked', true);
+$('.form-elements__checkbox-1:gt(0)').prop('checked', true);
+$('.form-elements__toggle').eq(0).prop('checked', true);
+$('.form-elements__2-like').prop('checked', true);
+$('.form-elements__checkbox-2:lt(4)').prop('checked', true);
+$('.form-elements__checkbox-2').eq(0).prop('checked', false);
+$('.form-elements__revew-1-like').prop('checked', true);
 $(document).ready(() => {
 
   $('.input__icon-dropdown').eq(1).trigger('click');
   $('.input__icon-dropdown').eq(2).trigger('click');
   $('.input__icon-dropdown').eq(3).trigger('click');
 
-  const $button = $('.form_elements__item_22 .iqdropdown button');
+  const $button = $('.form-elements__item_22 .iqdropdown button');
   $button.eq(1).trigger('click');
   $button.eq(1).trigger('click');
   $button.eq(3).trigger('click');
 
-  $('.form_elements__item_15').bind('DOMSubtreeModified', () => {
+  $('.form-elements__item_15').bind('DOMSubtreeModified', () => {
     const $paginationPage = $('.paginationjs-page');
     if ($paginationPage.length > 4) {
       $paginationPage.css({ 'margin-right': '-8px' });
