d2da9afd3f28ddaaa8ec21c087527b952310a5f5
parunkov <parunkov@gmail.com>
2020-05-16 10:27:20 +0500

    Исправил colors-types по БЭМ

--- source/pages/colors-types/colors-types.js
+++ source/pages/colors-types/colors-types.js
@@ -2,4 +2,4 @@ import '../../../node_modules/reset-css/sass/_reset.scss';
 import '../../variables.scss';
 import '../../components/page/page';
 import '../../components/ui-kit/ui-kit';
-import './colors_types.scss';
+import './colors-types.scss';
