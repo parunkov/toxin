fe2cc626e210e4943a6bd79774bba3b56ba5a386
parunkov <parunkov@gmail.com>
2020-05-17 08:39:40 +0500

    Исправил скрипт открытия мобильного меню #5

--- source/components/header/header.js
+++ source/components/header/header.js
@@ -45,13 +45,15 @@ $(document).click(
   },
 );
 
-$('.header__burger').click(
-  () => {
-    $('.header__ul-wrap').toggleClass('header__ul-wrap_show');
-    $('.header__login-container').toggleClass('header__login-container_show');
-    $('.header__login-container-logged').toggleClass('header__login-container_show');
-    $('.header__burger-line_burger').toggleClass('header__burger-line_hide');
-    $('.header__burger-line_cross-1').toggleClass('header__burger-line_show');
-    $('.header__burger-line_cross-2').toggleClass('header__burger-line_show');
-  },
-);
+$('.header__burger').each((i) => {
+  $('.header__burger').eq(i).click(
+    () => {
+      $('.header__ul-wrap').eq(i).toggleClass('header__ul-wrap_show');
+      $('.header__login-container').eq(i).toggleClass('header__login-container_show');
+      $('.header').eq(i).find('.header__login-container-logged').toggleClass('header__login-container_show');
+      $('.header__burger').eq(i).find('.header__burger-line_burger').toggleClass('header__burger-line_hide');
+      $('.header__burger').eq(i).find('.header__burger-line_cross-1').toggleClass('header__burger-line_show');
+      $('.header__burger').eq(i).find('.header__burger-line_cross-2').toggleClass('header__burger-line_show');
+    },
+  );
+});
