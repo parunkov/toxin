f74f672769e98f4cff08e7dc90c3f5aa5caf4a08
parunkov <parunkov@gmail.com>
2020-05-20 14:55:45 +0500

    Закешировал переменные в скрипте хедера

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,6 +5,9 @@ import './header.scss';
 import $ from 'jquery';
 
 const $header = $('.header');
+const $subMenu = $('.header__nav-list-submenu');
+const $itemWithSubMenu = $('.header__nav-item_with-submenu');
+const $burger = $('.header__burger');
 
 $header.each((i) => {
   const num = $header.eq(i).find('.header__active-page-number').html();
@@ -17,18 +20,17 @@ $header.each((i) => {
   });
 });
 
-
-$('.header__nav-item_with-submenu').click(
+$itemWithSubMenu.click(
   function (e) {
     if ($(e.target).parent().hasClass('header__nav-item_with-submenu')) {
       e.preventDefault();
     }
     if ($(this).find('.header__nav-list-submenu').hasClass('header__nav-list-submenu_visible')) {
-      $('.header__nav-list-submenu').removeClass('header__nav-list-submenu_visible');
-      $('.header__nav-item_with-submenu').removeClass('header__nav-item_with-submenu-open');
+      $subMenu.removeClass('header__nav-list-submenu_visible');
+      $itemWithSubMenu.removeClass('header__nav-item_with-submenu-open');
     } else {
-      $('.header__nav-list-submenu').removeClass('header__nav-list-submenu_visible');
-      $('.header__nav-item_with-submenu').removeClass('header__nav-item_with-submenu-open');
+      $subMenu.removeClass('header__nav-list-submenu_visible');
+      $itemWithSubMenu.removeClass('header__nav-item_with-submenu-open');
       $(this).find('.header__nav-list-submenu').addClass('header__nav-list-submenu_visible');
       console.log($(this));
       $(this).addClass('header__nav-item_with-submenu-open');
@@ -41,19 +43,19 @@ $(document).click(
     if ($(e.target).parent().hasClass('header__nav-item_with-submenu')) {
       e.preventDefault();
     } else {
-      $('.header__nav-list-submenu').removeClass('header__nav-list-submenu_visible');
-      $('.header__nav-item_with-submenu').removeClass('header__nav-item_with-submenu-open');
+      $subMenu.removeClass('header__nav-list-submenu_visible');
+      $itemWithSubMenu.removeClass('header__nav-item_with-submenu-open');
     }
   },
 );
 
-$('.header__burger').each((i) => {
-  $('.header__burger').eq(i).click(
+$burger.each((i) => {
+  $burger.eq(i).click(
     () => {
       $('.header__nav-list-wrap').eq(i).toggleClass('header__nav-list-wrap_visible');
       $('.header__btn-container').eq(i).toggleClass('header__btn-container_visible');
       $('.header').eq(i).find('.header__btn-container-logged').toggleClass('header__btn-container_visible');
-      $('.header__burger').eq(i).find('.header__burger-line').toggleClass('header__burger-line_cross');
+      $burger.eq(i).find('.header__burger-line').toggleClass('header__burger-line_cross');
     },
   );
 });
