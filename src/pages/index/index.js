9f9588ecf7e727ae96091215ec5256ec57cecc8e
parunkov <parunkov@gmail.com>
2020-05-22 08:30:10 +0500

    Исправи стили index

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,5 @@
 import '../../../node_modules/reset-css/sass/_reset.scss';
 import '../../variables.scss';
 import '../../components/page/page';
-import '../../components/page-header/page-header';
-import '../../components/search/search';
-import '../../components/page-footer/page-footer';
 import './index.scss';
 import '../../favicons/favicons';
