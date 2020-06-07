091a9bf432c921e6cd3d58a8cea129a0704cdd5b
parunkov <parunkov@gmail.com>
2020-06-07 17:58:27 +0500

    Исправил классы form-elements__item

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -17,23 +17,23 @@ import '../../components/review/review';
 import './form-elements.scss';
 import $ from 'jquery';
 
-$('.form-elements__item_8 .checkbox__input:gt(0)').prop('checked', true);
-$('.form-elements__item_10 .toggle__input').eq(0).prop('checked', true);
+$('.form-elements__column:nth-child(2) .form-elements__item:nth-child(1) .checkbox__input:gt(0)').prop('checked', true);
+$('.form-elements__column:nth-child(2) .form-elements__item:nth-child(3) .toggle__input').eq(0).prop('checked', true);
 $('.like__input').eq(1).prop('checked', true);
-$('.form-elements__item_19 .checkbox__input:lt(4)').prop('checked', true);
-$('.form-elements__item_19 .checkbox__input').eq(0).prop('checked', false);
+$('.form-elements__column:nth-child(5) .form-elements__item:nth-child(2) .checkbox__input:lt(4)').prop('checked', true);
+$('.form-elements__column:nth-child(5) .form-elements__item:nth-child(2) .checkbox__input').eq(0).prop('checked', false);
 $('.like__input').eq(2).prop('checked', true);
 $(document).ready(() => {
   $('.dropdown__input-icon').eq(1).trigger('click');
   $('.dropdown__input-icon').eq(2).trigger('click');
   $('.dropdown__input-icon').eq(3).trigger('click');
 
-  const $button = $('.form-elements__item_22 .iqdropdown button');
+  const $button = $('.form-elements__column:nth-child(8) .form-elements__item .iqdropdown button');
   $button.eq(1).trigger('click');
   $button.eq(1).trigger('click');
   $button.eq(3).trigger('click');
 
-  $('.form-elements__item_15').bind('DOMSubtreeModified', () => {
+  $('.form-elements__column:nth-child(3) .form-elements__item:nth-child(3)').bind('DOMSubtreeModified', () => {
     const $paginationPage = $('.paginationjs-page');
     if ($paginationPage.length > 4) {
       $paginationPage.css({ 'margin-right': '-8px' });
