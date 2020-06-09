3b1d0f654d079b1ce916c79843984a2a53a33793
parunkov <parunkov@gmail.com>
2020-06-09 09:00:50 +0500

    Добавил курсор pointer  между радио кнопкой и текстом #5

--- src/components/radio/radio.js
+++ src/components/radio/radio.js
@@ -1,3 +1,14 @@
 import '../../variables.scss';
 import './radio.scss';
 import './radio.pug';
+
+const $label = $('.radio__label');
+$label.mousemove(() => {
+	$label.each((i) => {
+		if ($label.eq(i).find('.radio__input').is(':checked')) {
+			$label.eq(i).css({'cursor' : 'default'});
+		} else {
+			$label.eq(i).css({'cursor' : 'pointer'});
+		}
+	});
+});
