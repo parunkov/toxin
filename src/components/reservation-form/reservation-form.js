6f65b775ecd5620217b20dea56d317c2fa91319e
parunkov <parunkov@gmail.com>
2020-06-17 11:23:55 +0500

    Исправил название dates-block #10

--- src/components/reservation-form/reservation-form.js
+++ src/components/reservation-form/reservation-form.js
@@ -1,5 +1,5 @@
 import '../../../node_modules/reset-css/sass/_reset.scss';
 import '../../variables.scss';
-import '../dates-block/dates-block'
+import '../dates-selector/dates-selector'
 import '../btn/btn';
 import './reservation-form.scss';
