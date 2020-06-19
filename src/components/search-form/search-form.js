beb3c0dad41967c8d024ab6e7ea120e589f1eb8d
parunkov <parunkov@gmail.com>
2020-06-19 11:45:39 +0500

    Исправил модификаторы кнопки #10

--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -4,7 +4,7 @@ import '../dates-selector/dates-selector'
 import '../btn/btn';
 import './search-form.scss';
 
-$('.search-form .btn_submit').click((e) => {
+$('.search-form .btn_theme_arrow').click((e) => {
 	e.preventDefault();
 	console.log(1);
 	window.location.href = 'search-room.html';
