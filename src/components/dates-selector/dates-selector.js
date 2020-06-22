eafc54e4d507e48d25e239a248e3e65d0190aa4f
parunkov <parunkov@gmail.com>
2020-06-22 11:05:47 +0500

    Заменил input на dropdown в dates-selector #10

--- src/components/dates-selector/dates-selector.js
+++ src/components/dates-selector/dates-selector.js
@@ -6,62 +6,68 @@ import '../datepicker-block/datepicker-block';
 import './dates-selector.scss';
 import $ from 'jquery';
 
-const $myDatepicker = $('.dates-selector__datepicker .datepicker-block__content').datepicker().data('datepicker');
-const $datepickerContainer = $('.dates-selector__datepicker');
-const $setBtn = $('.dates-selector__datepicker .datepicker-block__set-btn');
-const $clearBtn = $('.dates-selector__datepicker .datepicker-block__clear-btn');
-const $arrival = $('.dropdown_theme_date:first-child .dropdown__input');
-const $departure = $('.dropdown_theme_date:nth-child(2) .dropdown__input');
-const $icon = $('.dropdown_theme_date .dropdown__arrow');
-console.log($icon.text());
+$(document).ready(() => {
+  const $datesSelector = $('.dates-selector');
 
-const dateToValue = (date) => `${(`0${date.getDate()}`).substr(-2)}.${(`0${date.getMonth() + 1}`).substr(-2)}.${date.getFullYear()}`;
+  $datesSelector.each((i) => {
 
-$arrival.attr('disabled', '');
-$departure.attr('disabled', '');
+    const $myDatepicker = $datesSelector.eq(i).find('.dates-selector__datepicker .datepicker-block__content').datepicker().data('datepicker');
+    const $datepickerContainer = $datesSelector.eq(i).find('.dates-selector__datepicker');
+    const $setBtn = $datesSelector.eq(i).find('.dates-selector__datepicker .datepicker-block__set-btn');
+    const $clearBtn = $datesSelector.eq(i).find('.dates-selector__datepicker .datepicker-block__clear-btn');
+    const $arrival = $datesSelector.eq(i).find('.dropdown_theme_date:first-child .dropdown__input');
+    const $departure = $datesSelector.eq(i).find('.dropdown_theme_date:nth-child(2) .dropdown__input');
+    const $icon = $datesSelector.eq(i).find('.dropdown_theme_date .dropdown__arrow');
 
-$icon.click(function (evt) {
-  evt.preventDefault();
-  if ($datepickerContainer.hasClass('dates-selector__datepicker_visible') && $(this).text() === 'expand_more') {
-    $icon.text('expand_less');
-    $(this).text('expand_more');
-  } else if ($datepickerContainer.hasClass('dates-selector__datepicker_visible') && $(this).text() === 'expand_less') {
-    $(this).text('expand_more');
-    $datepickerContainer.removeClass('dates-selector__datepicker_visible');
-    $icon.removeClass('dropdown__arrow_cursor_default');
-  } else if (!$datepickerContainer.hasClass('dates-selector__datepicker_visible') && $(this).text() === 'expand_less') {
-    $datepickerContainer.addClass('dates-selector__datepicker_visible');
-    $icon.text('expand_more');
-    $(this).text('expand_less');
-    $(this).removeClass('dropdown__arrow_cursor_default');
-  } else {
-    $datepickerContainer.addClass('dates-selector__datepicker_visible');
-    $icon.text('expand_more');
-    $icon.addClass('dropdown__arrow_cursor_default');
-    $(this).text('expand_less');
-    $(this).removeClass('dropdown__arrow_cursor_default');
-  }
+    const dateToValue = (date) => `${(`0${date.getDate()}`).substr(-2)}.${(`0${date.getMonth() + 1}`).substr(-2)}.${date.getFullYear()}`;
 
-  $setBtn.click((e) => {
-    e.preventDefault();
-    $datepickerContainer.removeClass('dates-selector__datepicker_visible');
-    $icon.removeClass('dropdown__arrow_cursor_default');
-    $icon.text('expand_more');
-    if ($myDatepicker.selectedDates[0]) {
-      $arrival.attr('value', dateToValue($myDatepicker.selectedDates[0]));
-    } else {
-      $arrival.attr('value', '');
-    }
-    if ($myDatepicker.selectedDates[1]) {
-      $departure.attr('value', dateToValue($myDatepicker.selectedDates[1]));
-    } else {
-      $departure.attr('value', '');
-    }
-  });
-  $clearBtn.click((e) => {
-    e.preventDefault();
-    $arrival.attr('value', '');
-    $departure.attr('value', '');
-  });
+    $arrival.attr('disabled', '');
+    $departure.attr('disabled', '');
+
+    $icon.click(function (evt) {
+      evt.preventDefault();
+      if ($datepickerContainer.hasClass('dates-selector__datepicker_visible') && $(this).text() === 'expand_more') {
+        $icon.text('expand_less');
+        $(this).text('expand_more');
+      } else if ($datepickerContainer.hasClass('dates-selector__datepicker_visible') && $(this).text() === 'expand_less') {
+        $(this).text('expand_more');
+        $datepickerContainer.removeClass('dates-selector__datepicker_visible');
+        $icon.removeClass('dropdown__arrow_cursor_default');
+      } else if (!$datepickerContainer.hasClass('dates-selector__datepicker_visible') && $(this).text() === 'expand_less') {
+        $datepickerContainer.addClass('dates-selector__datepicker_visible');
+        $icon.text('expand_more');
+        $(this).text('expand_less');
+        $(this).removeClass('dropdown__arrow_cursor_default');
+      } else {
+        $datepickerContainer.addClass('dates-selector__datepicker_visible');
+        $icon.text('expand_more');
+        $icon.addClass('dropdown__arrow_cursor_default');
+        $(this).text('expand_less');
+        $(this).removeClass('dropdown__arrow_cursor_default');
+      }
+
+      $setBtn.click((e) => {
+        e.preventDefault();
+        $datepickerContainer.removeClass('dates-selector__datepicker_visible');
+        $icon.removeClass('dropdown__arrow_cursor_default');
+        $icon.text('expand_more');
+        if ($myDatepicker.selectedDates[0]) {
+          $arrival.attr('value', dateToValue($myDatepicker.selectedDates[0]));
+        } else {
+          $arrival.attr('value', '');
+        }
+        if ($myDatepicker.selectedDates[1]) {
+          $departure.attr('value', dateToValue($myDatepicker.selectedDates[1]));
+        } else {
+          $departure.attr('value', '');
+        }
+      });
+      $clearBtn.click((e) => {
+        e.preventDefault();
+        $arrival.attr('value', '');
+        $departure.attr('value', '');
+      });
+    });
+  });  
+
 });
-
