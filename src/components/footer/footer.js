30f6114fa9c90cd886d66025eaf3fa9d5e7b0c6b
parunkov <parunkov@gmail.com>
2020-07-10 18:19:30 +0500

    Вынес данные header и footer в JSON

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,3 +3,78 @@ import '../../variables.scss';
 import '../input/input';
 import '../logo/logo';
 import './footer.scss';
+
+
+
+
+// const MenuItem = function(item, href, incl, hrefs) {
+// 	this.item = item;
+// 	this.href = href;
+// 	this.incl = incl;
+// 	this.hrefs = hrefs;
+// }
+// const navRus = [
+// new MenuItem('навигация', '/mock-address/change-me', ['О нас', 'Новости', 'Служба поддержки', 'Услуги'], ['/mock-address/change-me', '/mock-address/change-me', '/mock-address/change-me', '/mock-address/change-me']),
+// new MenuItem('о нас', '/mock-address/change-me', ['О сервисе', 'Наша команда', 'Вакансии', 'Инвесторы'], ['/mock-address/change-me', '/mock-address/change-me', '/mock-address/change-me', '/mock-address/change-me']),
+// new MenuItem('Служба поддержки', '/mock-address/change-me', ['Соглашения', 'Сообщества', 'Связь с нами'], ['/mock-address/change-me', '/mock-address/change-me', '/mock-address/change-me'])
+// ];
+// const navEng = [
+// new MenuItem('Navigation', '/mock-address/change-me', ['About Us', 'News', 'Support', 'Products'], ['/mock-address/change-me', '/mock-address/change-me', '/mock-address/change-me', '/mock-address/change-me']),
+// new MenuItem('About Us', '/mock-address/change-me', ['Who we are', 'Our team', 'Careers', 'Investors'], ['/mock-address/change-me', '/mock-address/change-me', '/mock-address/change-me', '/mock-address/change-me']),
+// new MenuItem('Support', '/mock-address/change-me', ['Documentation', 'Community', 'Get in Touch'], ['/mock-address/change-me', '/mock-address/change-me', '/mock-address/change-me'])
+// ];
+// const inputEng = {
+// 	h3: '',
+// 	type: 'email',
+// 	name: 'email',
+// 	cl: ' input__field_with-arrow',
+// 	val: '',
+// 	plac: 'Your email address',
+// 	p: '',
+// 	icon: 'arrow_forward',
+// 	color: 'purple'
+// }
+// const inputRus = {
+// 	h3: '',
+// 	type: 'email',
+// 	name: 'email',
+// 	cl: ' input__field_with-arrow',
+// 	val: '',
+// 	plac: 'Email',
+// 	p: '',
+// 	icon: 'arrow_forward',
+// 	color: 'purple'
+// }
+// const markupFooterEng = {
+// 	cl: '',
+// 	text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit aliquam eget nullam pellentesque aliquam curabitur cociis.',
+// 	menu: navEng,
+// 	h3: 'Subscribe to our newsletter',
+// 	p: 'Receive our latest news and promotions in your inbox!',
+// 	copyright: 'Copyright © 2018 Toxin UI Kit. All rights reserved.',
+// 	ui: 1
+// }
+// const markupFooterRus = {
+// 	cl: '',
+// 	text: 'Бронирование номеров в лучшем отеле 2019 года по версии ассоциации «Отельные взгляды»',
+// 	menu: navRus,
+// 	h3: 'Подписка',
+// 	p: 'Получайте специальные предложения и новости сервиса',
+// 	copyright: 'Copyright © 2018 Toxin отель. Все права зачищены.'
+// }
+
+// const data = {
+// 	markupFooterEng,
+// 	markupFooterRus,
+// 	inputRus,
+// 	inputEng
+// }
+
+// console.log(JSON.stringify(data));
+
+
+
+
+
+// const footerData = require('../../commonData/footer.json');
+// console.log(footerData);
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -102,3 +102,66 @@ $('.header').each((i) => {
 //     },
 //   );
 // });
+
+
+
+
+
+
+// const MenuItem = function(item, href, incl, hrefs) {
+//     this.item = item;
+//     this.href = href;
+//     this.incl = incl;
+//     this.hrefs = hrefs;
+//   }
+// const menuRus = [
+//     new MenuItem('О нас', '/mock-address/change-me'),
+//     new MenuItem('Услуги', '/mock-address/change-me', ['Услуга 1', 'Услуга 2', 'Услуга 3'], ['/mock-address/change-me', "/mock-address/change-me", '/mock-address/change-me']),
+//     new MenuItem('Вакансии', '/mock-address/change-me'),
+//     new MenuItem('Новости', '/mock-address/change-me'),
+//     new MenuItem('Соглашения', '/mock-address/change-me', ['Соглашение 1', 'Соглашение 2', 'Соглашение 3'], ['/mock-address/change-me', "/mock-address/change-me", '/mock-address/change-me']),
+//   ];
+//   const menuEng = [
+//     new MenuItem('Home', '/mock-address/change-me'),
+//     new MenuItem('About Us', '/mock-address/change-me'),
+//     new MenuItem('Services', '/mock-address/change-me', ['Service 1', 'Service 2', 'Service 3'], ['/mock-address/change-me', "/mock-address/change-me", '/mock-address/change-me']),
+//     new MenuItem('Careers', '/mock-address/change-me'),
+//     new MenuItem('News', '/mock-address/change-me'),
+//     new MenuItem('Documentation', '/mock-address/change-me')
+//   ];
+//   const markupEng1 = {
+//     cl: '',
+//     menu: menuEng,
+//     btn1: 'Login',
+//     href1: '/mock-address/change-me',
+//     btn2: 'Register',
+//     href2: '/mock-address/change-me',
+//     login: 1
+//   }
+//   const markupEng2 = {
+//     cl: '',
+//     menu: menuEng,
+//     btn1: 'Login',
+//     href1: '/mock-address/change-me',
+//     btn2: 'Register',
+//     href2: '/mock-address/change-me'
+//   }
+//   const markupRus = {
+//     cl: '',
+//     menu: menuRus,
+//     btn1: 'войти',
+//     href1: 'sign-in.html',
+//     btn2: 'зарегистрироваться',
+//     href2: 'registration.html'
+//   }
+// const headerData = {
+//   markupEng1,
+//   markupEng2,
+//   markupRus
+// }
+// console.log(JSON.stringify(headerData));
+// const json = JSON.stringify(headerData);
+// const json = require('../../commonData/header.json');
+// console.log(json);
+// const jsonParse = JSON.parse(json);
+// console.log(jsonParse);
