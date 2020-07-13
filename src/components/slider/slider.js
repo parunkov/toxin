3ca8f56a3fc6e5ae27ac22cb9d7713818996184c
parunkov <parunkov@gmail.com>
2020-07-13 10:38:29 +0500

    Добавил префикс js в slider

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -42,29 +42,4 @@ class Slider {
   }
 }
 
-const slider = new Slider($('.slider__container'));
-
-
-
-
-
-// function prettify(num) {
-//   const n = num.toString();
-//   const separator = ' ';
-//   return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, `$1${separator}`);
-// }
-
-// $(() => {
-//   $('#slider__range').slider({
-//     range: true,
-//     min: 0,
-//     max: 17000,
-//     step: 100,
-//     values: [5000, 10000],
-//     slide(event, ui) {
-//       $('#slider__amount').val(`${prettify(ui.values[0])}₽ - ${prettify(ui.values[1])}₽`);
-//     },
-//   });
-//   $('#slider__amount').val(`${prettify($('#slider__range').slider('values', 0))}₽`
-//     + ` - ${prettify(`${$('#slider__range').slider('values', 1)}₽`)}`);
-// });
+const slider = new Slider($('.js-slider'));
