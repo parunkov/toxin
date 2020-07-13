8919a714c3f2c1e6691f8b43d992fd1ffc3956e6
parunkov <parunkov@gmail.com>
2020-07-13 10:13:36 +0500

    Добавил префикс JS в radio

--- src/components/radio/radio.js
+++ src/components/radio/radio.js
@@ -5,33 +5,19 @@ import './radio.pug';
 class Radio {
 	constructor(radio) {
 		this.$radio = radio;
-		this.$label = radio.find('.radio__label');
-		this.$input = radio.find('.radio__input');
+		this.$label = radio.find('.js-radio__label');
+		this.$input = radio.find('.js-radio__input');
 		this.init();
 	}
 	init() {
 		const onMoouseMove = () => {
 			if (this.$input.is(':checked')) {
-				this.$label.removeClass('radio__label_cursor_pointer');
+				this.$label.removeClass('js-radio__label_cursor_pointer');
 			} else {
-				this.$label.addClass('radio__label_cursor_pointer');
+				this.$label.addClass('js-radio__label_cursor_pointer');
 			}
 		}
 	}
 }
 
-const radio = new Radio($('.radio'));
-
-
-
-
-// const $label = $('.radio__label');
-// $label.mousemove(() => {
-// 	$label.each((i) => {
-// 		if ($label.eq(i).find('.radio__input').is(':checked')) {
-// 			$label.eq(i).removeClass('radio__label_cursor_pointer');
-// 		} else {
-// 			$label.eq(i).addClass('radio__label_cursor_pointer');
-// 		}
-// 	});
-// });
+const radio = new Radio($('.js-radio'));
