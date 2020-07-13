73a4863af5806bbff07b6e064af345c22d0528ab
parunkov <parunkov@gmail.com>
2020-07-13 08:57:20 +0500

    Добавил префикс js в card

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -7,10 +7,10 @@ import $ from 'jquery';
 class Card {
   constructor(card) {
     this.$card = card;
-    this.$slides = card.find('.card__slide');
-    this.$left = card.find('.card__arrow_left');
-    this.$right = card.find('.card__arrow_right');
-    this.$controls = card.find('.card__control');
+    this.$slides = card.find('.js-card__slide');
+    this.$left = card.find('.js-card__arrow_left');
+    this.$right = card.find('.js-card__arrow_right');
+    this.$controls = card.find('.js-card__control');
     this.counter = 0;
     this.init();
   }
@@ -45,34 +45,34 @@ class Card {
     });
   }
   showSlides() {
-    this.$slides.removeClass('card__slide-left');
-    this.$slides.removeClass('card__slide-active');
-    this.$slides.removeClass('card__slide-right');
-    this.$controls.removeClass('card__control_active');
+    this.$slides.removeClass('js-card__slide-left');
+    this.$slides.removeClass('js-card__slide-active');
+    this.$slides.removeClass('js-card__slide-right');
+    this.$controls.removeClass('js-card__control_active');
     switch (this.counter) {
       case 0:
-      this.$slides.eq(3).addClass('card__slide-left');
-      this.$slides.eq(0).addClass('card__slide-active');
-      this.$slides.eq(1).addClass('card__slide-right');
-      this.$controls.eq(0).addClass('card__control_active');
+      this.$slides.eq(3).addClass('js-card__slide-left');
+      this.$slides.eq(0).addClass('js-card__slide-active');
+      this.$slides.eq(1).addClass('js-card__slide-right');
+      this.$controls.eq(0).addClass('js-card__control_active');
       break;
       case 1:
-      this.$slides.eq(0).addClass('card__slide-left');
-      this.$slides.eq(1).addClass('card__slide-active');
-      this.$slides.eq(2).addClass('card__slide-right');
-      this.$controls.eq(1).addClass('card__control_active');
+      this.$slides.eq(0).addClass('js-card__slide-left');
+      this.$slides.eq(1).addClass('js-card__slide-active');
+      this.$slides.eq(2).addClass('js-card__slide-right');
+      this.$controls.eq(1).addClass('js-card__control_active');
       break;
       case 2:
-      this.$slides.eq(1).addClass('card__slide-left');
-      this.$slides.eq(2).addClass('card__slide-active');
-      this.$slides.eq(3).addClass('card__slide-right');
-      this.$controls.eq(2).addClass('card__control_active');
+      this.$slides.eq(1).addClass('js-card__slide-left');
+      this.$slides.eq(2).addClass('js-card__slide-active');
+      this.$slides.eq(3).addClass('js-card__slide-right');
+      this.$controls.eq(2).addClass('js-card__control_active');
       break;
       case 3:
-      this.$slides.eq(2).addClass('card__slide-left');
-      this.$slides.eq(3).addClass('card__slide-active');
-      this.$slides.eq(0).addClass('card__slide-right');
-      this.$controls.eq(3).addClass('card__control_active');
+      this.$slides.eq(2).addClass('js-card__slide-left');
+      this.$slides.eq(3).addClass('js-card__slide-active');
+      this.$slides.eq(0).addClass('js-card__slide-right');
+      this.$controls.eq(3).addClass('js-card__control_active');
       break;
       default:
       break;
@@ -80,84 +80,5 @@ class Card {
   }
 }
 
-// const cards = [];
-// $('.card').each((i) => {
-//   cards[i] = new Card($('.card').eq(i));
-// });
-
 export default Card;
-
-
-
-
-
-// $('.card').each((i) => {
-//   let counter = 0;
-//   const $slides = $('.card').eq(i).find('.card__slide');
-//   const $left = $('.card').eq(i).find('.card__arrow_left');
-//   const $right = $('.card').eq(i).find('.card__arrow_right');
-//   const $controls = $('.card').eq(i).find('.card__control');
-//   const showSlides = function () {
-//     $slides.removeClass('card__slide-left');
-//     $slides.removeClass('card__slide-active');
-//     $slides.removeClass('card__slide-right');
-//     $controls.removeClass('card__control_active');
-//     switch (counter) {
-//       case 0:
-//         $slides.eq(3).addClass('card__slide-left');
-//         $slides.eq(0).addClass('card__slide-active');
-//         $slides.eq(1).addClass('card__slide-right');
-//         $controls.eq(0).addClass('card__control_active');
-//         break;
-//       case 1:
-//         $slides.eq(0).addClass('card__slide-left');
-//         $slides.eq(1).addClass('card__slide-active');
-//         $slides.eq(2).addClass('card__slide-right');
-//         $controls.eq(1).addClass('card__control_active');
-//         break;
-//       case 2:
-//         $slides.eq(1).addClass('card__slide-left');
-//         $slides.eq(2).addClass('card__slide-active');
-//         $slides.eq(3).addClass('card__slide-right');
-//         $controls.eq(2).addClass('card__control_active');
-//         break;
-//       case 3:
-//         $slides.eq(2).addClass('card__slide-left');
-//         $slides.eq(3).addClass('card__slide-active');
-//         $slides.eq(0).addClass('card__slide-right');
-//         $controls.eq(3).addClass('card__control_active');
-//         break;
-//       default:
-//         break;
-//     }
-//   };
-//   showSlides();
-//   $controls.each((j) => {
-//     $controls.eq(j).click((evt) => {
-//       evt.preventDefault();
-//       counter = j;
-//       showSlides();
-//     });
-//   });
-//   $left.click(() => {
-//     switch (counter) {
-//       case 0:
-//         counter = 3;
-//         break;
-//       default:
-//         counter -= 1;
-//     }
-//     showSlides();
-//   });
-//   $right.click(() => {
-//     switch (counter) {
-//       case 3:
-//         counter = 0;
-//         break;
-//       default:
-//         counter += 1;
-//     }
-//     showSlides();
-//   });
-// });
 
