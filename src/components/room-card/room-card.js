9f7023859894ba513b4a098947ada1a0f1ac1825
parunkov <parunkov@gmail.com>
2020-07-31 09:15:45 +0500

    Сделал карточку для любого количества картинок #21

--- src/components/room-card/room-card.js
+++ src/components/room-card/room-card.js
@@ -8,38 +8,56 @@ class Card {
   constructor(card) {
     this.$card = card;
     this.$slides = card.find('.js-room-card__slide');
+    this.length = this.$slides.length;
     this.$left = card.find('.js-room-card__arrow_left');
     this.$right = card.find('.js-room-card__arrow_right');
     this.$controls = card.find('.js-room-card__control');
     this.counter = 0;
+    this.numberLeftSlide = this.length;
+    this.numberRightSlide = 1;
     this.init();
   }
   init() {
+    const setSlidesNumbers = (j) => {
+      this.numberLeftSlide = j - 1;
+      this.numberRightSlide = j + 1;
+      if (this.counter === 0) {
+        this.numberLeftSlide = this.length - 1;
+      }
+      if (this.counter === (this.length - 1)) {
+        this.numberRightSlide = 0;
+      }
+    }
     this.showSlides();
     this.$controls.each((j) => {
       this.$controls.eq(j).click((evt) => {
         evt.preventDefault();
         this.counter = j;
+        setSlidesNumbers(this.counter);
         this.showSlides();
       });
     });
     this.$left.click(() => {
       switch (this.counter) {
         case 0:
-        this.counter = 3;
+        this.counter = this.length - 1;
+        setSlidesNumbers(this.counter);
         break;
         default:
         this.counter -= 1;
+        setSlidesNumbers(this.counter);
       }
       this.showSlides();
     });
     this.$right.click(() => {
       switch (this.counter) {
-        case 3:
+        case (this.length - 1):
         this.counter = 0;
+        setSlidesNumbers(this.counter);
         break;
         default:
         this.counter += 1;
+        setSlidesNumbers(this.counter);
       }
       this.showSlides();
     });
@@ -56,22 +74,8 @@ class Card {
     this.$slides.removeClass('room-card__slide-active');
     this.$slides.removeClass('js-room-card__slide-right');
     this.$controls.removeClass('room-card__control_active');
-    switch (this.counter) {
-      case 0:
-      changeSlides(3, 0, 1);
-      break;
-      case 1:
-      changeSlides(0, 1, 2);
-      break;
-      case 2:
-      changeSlides(1, 2, 3);
-      break;
-      case 3:
-      changeSlides(2, 3, 0);
-      break;
-      default:
-      break;
-    }
+    // console.log(this.numberLeftSlide + ' ' + this.counter + ' ' + this.numberRightSlide);
+    changeSlides(this.numberLeftSlide, this.counter, this.numberRightSlide);
   }
 }
 
